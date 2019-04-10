385738c2b64b90d7af5986a6aea4ffbf9880583e
ssingularity <1755405701@qq.com>
2019-04-10 18:46:09 +0800

    finish initialize test

--- src/test/java/sample/LunarTest.java
+++ src/test/java/sample/LunarTest.java
@@ -7,20 +7,19 @@ import org.junit.Test;
  * 农历示例
  */
 public class LunarTest {
-
-  @Test
-  public void test(){
-    //今天
-    Lunar date = new Lunar();
-    //输出阴历信息
-    System.out.println(date.toFullString());
-    //输出阳历信息
-    System.out.println(date.getSolar().toFullString());
-    System.out.println();
-    //指定阴历的某一天
-    date = new Lunar(1986,4,21);
-    System.out.println(date.toFullString());
-    System.out.println(date.getSolar().toFullString());
+  @Test(expected = Exception.class)
+  public void givenNegativeDayWhenToStringThenThrowException(){
+      Lunar date = new Lunar(2000, 1, -1);
+      date.toString();
   }
 
+  @Test(expected = Exception.class)
+  public void givenNegativeMonthWhenInitializeThenThrowException(){
+      Lunar date = new Lunar(2000, -1, 20);
+  }
+
+  @Test(expected = Exception.class)
+  public void givenNegativeYearWhenInitializeThenThrowException(){
+      Lunar date = new Lunar(-1, 10, 20);
+  }
 }
