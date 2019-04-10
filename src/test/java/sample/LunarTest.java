d068a114d7d0474c0f10123a14512debe85f31a3
ssingularity <1755405701@qq.com>
2019-04-10 19:00:35 +0800

    finish LunarTest

--- src/test/java/sample/LunarTest.java
+++ src/test/java/sample/LunarTest.java
@@ -1,6 +1,8 @@
 package sample;
 
 import com.nlf.calendar.Lunar;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,33 +10,40 @@ import static org.junit.Assert.assertEquals;
  * 农历示例
  */
 public class LunarTest {
+    @Test
+    public void givenLunarAndLunarSolarInSameYearWhenConvertToSolarThenEquals() {
+      Lunar date = new Lunar(1986, 4, 21);
+      assertEquals("1986-05-29", date.getSolar().toString());
+    }
 
-    private Object[][] data = new Object[][]{
-            {1986, 4, 21, "1986-05-30"}
-    };
-
-  @Test
-  public void givenLunarWhenConverToSolarThenEquals() {
-      Lunar date = new Lunar(Integer.parseInt(data[0][0].toString()), Integer.parseInt(data[0][1].toString()), Integer.parseInt(data[0][2].toString()));
-      assertEquals(data[0][3].toString(), date.getSolar().next(1).toString());
-  }
+    @Test
+    public void givenLunarAndLunarSolarNotInSameYearWhenConvertToSolarThenEquals() {
+        Lunar date = new Lunar(1986, 12, 3);
+        assertEquals("1987-01-02", date.getSolar().toString());
+    }
 
 
-  @Test(expected = Exception.class)
-  public void givenNegativeMonthWhenInitializeThenThrowException(){
+    @Test(expected = Exception.class)
+    public void givenNegativeMonthWhenInitializeThenThrowException(){
       Lunar date = new Lunar(2000, -1, 20);
-  }
+    }
 
 
-  @Test(expected = Exception.class)
-  public void givenNegativeYearWhenInitializeThenThrowException(){
+    @Test(expected = Exception.class)
+    public void givenNegativeYearWhenInitializeThenThrowException(){
       Lunar date = new Lunar(-1, 10, 20);
-  }
-  
-  @Test(expected = Exception.class)
-  public void givenNegativeDayWhenToStringThenThrowException(){
+    }
+
+    @Test(expected = Exception.class)
+    public void givenNegativeDayWhenToStringThenThrowException(){
       Lunar date = new Lunar(2000, 1, -1);
       date.toString();
-  }
+    }
+
+    @Test
+    public void givenLunarDateWhenToFullStringThenSuccess(){
+      Lunar date = new Lunar(2018, 2, 2);
+      Assert.assertEquals("戊戌年贰月初二 狗年 (龙头节) 西方白虎 娄金狗", date.toFullString());
+    }
 
 }
